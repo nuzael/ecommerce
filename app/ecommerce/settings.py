2e79ce0567d320be86b9708aead139218754b617
nuzael <juulioemanuel@gmail.com>
2023-01-06 11:59:15 -0300

    refactor: change media file path

--- app/ecommerce/settings.py
+++ app/ecommerce/settings.py
@@ -125,14 +125,15 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'templates/static'
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
